6c3d63a65e9e8974516ed6ae6f662105e0a03476
hisham waleed karam <hisham.karam@cartologic.com>
2017-08-11 16:24:01 +0200

    use new Standard app views from cartoview

--- urls.py
+++ urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, url
 from . import APP_NAME, views
 
 urlpatterns = patterns('',
-                       url(r'^new/$', views.new, name='%s.new' % APP_NAME),
+                       url(r'^new/$', views.feature_list.new,
+                           name='%s.new' % APP_NAME),
                        url(r'^(?P<instance_id>\d+)/edit/$',
-                           views.edit, name='%s.edit' % APP_NAME),
+                           views.feature_list.edit, name='%s.edit' % APP_NAME),
                        url(r'^(?P<instance_id>\d+)/view/$',
-                           views.view_app, name='%s.view' % APP_NAME)
+                           views.feature_list.view_app, name='%s.view' % APP_NAME)
                        )
