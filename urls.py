9a691c8536a9ba6cd9dcf5dbfddb9b92ca0c4b4c
Mohamed Radwan <mohamed.radwan@cartologic.com>
2020-06-22 19:15:05 +0200

    change url to path django 2.2

--- urls.py
+++ urls.py
@@ -1,12 +1,9 @@
-from django.conf.urls import url
-
+from django.urls import path
 from . import APP_NAME, views
 
 urlpatterns = [
-    url(r'^new/$', views.feature_list.new,
-        name='%s.new' % APP_NAME),
-    url(r'^(?P<instance_id>\d+)/edit/$',
-        views.feature_list.edit, name='%s.edit' % APP_NAME),
-    url(r'^(?P<instance_id>\d+)/view/$',
-        views.feature_list.view_app, name='%s.view' % APP_NAME)
+    path('new/', views.feature_list.new, name='%s.new' % APP_NAME),
+    path('<int:instance_id>/edit/', views.feature_list.edit, name='%s.edit' % APP_NAME),
+    path('<int:instance_id>/view/', views.feature_list.view_app, name='%s.view' % APP_NAME)
 ]
+
